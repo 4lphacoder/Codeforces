e32852c193f5a10c3f55d6e024f98fa1d3330e27
Prashant Varshney <pv03158@gmail.com>
2020-10-03 16:49:55 +0530

    solved problem

---
+++ Ladder - 1300/Word.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cctype>
+#include <algorithm>
+
+using namespace std;
+int
+main(int argc, char* argv[]) {
+    string strbuff;
+    cin >> strbuff;
+
+    int uppercnt = 0, lowercnt = 0;
+    for(char ch : strbuff)
+        if(isupper(ch)) uppercnt ++;
+        else lowercnt ++;
+
+    if(uppercnt <= lowercnt) 
+        transform(strbuff.begin(), strbuff.end(), strbuff.begin(), ::tolower);
+    else 
+        transform(strbuff.begin(), strbuff.end(), strbuff.begin(), ::toupper);
+
+    cout << strbuff ;
+    return 0;
+}
